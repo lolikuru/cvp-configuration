91b7b5401017048b329c0899d557c22ee1ff4ae8
Pavel Glazov <pglazov@mirantis.com>
2019-08-26 12:12:15 +0400

    Fix repo whith proxy

--- configure.sh
+++ configure.sh
@@ -48,7 +48,7 @@ tempest_configuration () {
     apt-get update; apt-get install -y iputils-ping curl wget
     rally verify create-verifier --name tempest_verifier_$sub_name --type tempest --source $TEMPEST_REPO --version $tempest_version
     rally verify add-verifier-ext --version 7a4bff728fbd8629ec211669264ab645aa921e2b --source https://github.com/openstack/telemetry-tempest-plugin
-    rally verify add-verifier-ext --version 12b770e923060f5ef41358c37390a25be56634f0 --source https://github.com/openstack/heat-tempest-plugin
+    rally verify add-verifier-ext --version 485e3d18ac4cd432fee798c6429569a84e220e37 --source https://gerrit.mcp.mirantis.com/packaging/sources/heat-tempest-plugin
     pip install --force-reinstall python-cinderclient==3.2.0
     unset https_proxy
   fi
